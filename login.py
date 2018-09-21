91fe0d21c89bc5b16e7650fb1d1da78b6a1fcfe0
kehlin swain <kehlinswain@kehlins-mbp.attlocal.net>
2018-09-21 18:31:33 -0500

    updates with flash login app able to login and redirect to athletes list page

--- login.py
+++ login.py
@@ -1,8 +1,9 @@
-from flask import  Flask, render_template, redirect, url_for, request
+from flask import  Flask, render_template, redirect, url_for, request, session
 import requests
 
 
 app = Flask(__name__)
+app.secret_key = "precious"
 
 #use decorators
 @app.route('/login', methods=['GET','POST'])
@@ -16,8 +17,16 @@ def login():
         if token.status_code != 200:
             return "error " + str(token.status_code)
         else:
-            return "You have successfully logged in"
+            session['access_token']=token.json()['token']
+            return redirect(url_for('athletes'))
     return render_template('login.html', error=error)
+
+
+@app.route('/athletes', methods=['GET','POST'])
+def athletes():
+    return "List of Athletes Here or opportunity to create new athlete"
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
